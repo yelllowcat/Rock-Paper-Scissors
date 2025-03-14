679d939cebaeaa1e367bb7b52ee23d284c3b67cd
David <davigonzalezdev@gmail.com>
2025-03-14 15:55:58 -0700

    Ending the logic of the game in scripts.js
    The game wasn't working whitout this changes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,115 @@
-let computer =["scissor","paper","rock"];
-let random =Math.floor(Math.random() * 3);
-console.log(computer[random])
+let choices = ["scissor", "paper", "rock"];
 
-function playerChoice ()  {
-    let response =prompt("select a option 0:scissor 1:paper, 3:rock");
-    let election = computer[response];
-    console.log(election);;
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3);
+  let choice = choices[random];
+  return choice;
 }
 
-playerChoice()
+function playerChoice() {
+  let response = prompt("choice your option: scissor, paper, rock");
+  response.toLowerCase;
+  return response;
+}
+
+let humanSelection = playerChoice();
+let ComputerSelection = getComputerChoice();
+
+function playRound(computer, human) {
+  console.log("computer choice " + computer);
+  console.log("you choice " + human);
+
+  if (computer == "paper" && human == "rock") {
+    console.log("computer win");
+    computerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "rock" && human == "rock") {
+    console.log("draw");
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "scissor" && human == "rock") {
+    console.log("player win");
+    playerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "scissor" && human == "paper") {
+    console.log("computer win");
+    computerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "scissor" && human == "scissor") {
+    console.log("draw");
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "rock" && human == "scissor") {
+    console.log("computer win");
+    computerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "rock" && human == "paper") {
+    console.log("player win");
+    playerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "paper" && human == "paper") {
+    console.log("draw");
+
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+  if (computer == "paper" && human == "scissor") {
+    console.log("player win");
+    playerScore++;
+    console.log(
+      "player score: " + playerScore + " computer score: " + computerScore
+    );
+  }
+}
+function playGame() {
+  playRound(ComputerSelection, humanSelection);
+  console.log("");
+  humanSelection = playerChoice();
+  ComputerSelection = getComputerChoice();
+  playRound(ComputerSelection, humanSelection);
+
+  console.log("");
+  humanSelection = playerChoice();
+  ComputerSelection = getComputerChoice();
+  playRound(ComputerSelection, humanSelection);
+  console.log("");
+  humanSelection = playerChoice();
+  ComputerSelection = getComputerChoice();
+  playRound(ComputerSelection, humanSelection);
+  console.log("");
+  humanSelection = playerChoice();
+  ComputerSelection = getComputerChoice();
+  playRound(ComputerSelection, humanSelection);
+
+  console.log("");
+  if (computerScore > playerScore) {
+    console.log("computer win");
+  } else {
+    console.log("player win");
+  }
+}
+
+playGame();
